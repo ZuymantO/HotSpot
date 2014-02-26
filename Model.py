22ce96ed16d167e0e8b4d574494d0a711e0bad60
J. Charles N. MBIADA <JC@openroam-prg-sg-1-172-14.net.univ-paris-diderot.fr>
2014-02-26 16:26:25 +0100

    Debut de transformation en classe de module

--- Model.py
+++ Model.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import wikipedia
-import tfidf
+import wikipedia as wiki
+import tfidf as tf
 from pymongo import MongoClient
-client = MongoClient()
-db = client.test_database
-articles = db.articles
+
+class Model:
+
+    def __init__(self, dbhost = "localhost", dbport=27017):
+        
+        self.client = MongoClient(dbhost, dbport)
+        self.db = client.test_database
+        articles = db.articles
+        self.tfidf = tf.TfIdf()
+
+    def getCurrentTF():
+        return self.tfidf
+    
+    def getArticlesTable(): # should never be used in real (purpose of test)
+        return self.articles
+
+
+
 
 
