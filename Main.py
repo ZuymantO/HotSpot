b0fc8992599e77ce9a8827d0cf38a5f6cf3d50db
Souhire KENAWI <souhirekenawi@gmail.com>
2014-03-13 18:38:09 +0100

    tfidf

--- Main.py
+++ Main.py
@@ -9,21 +9,36 @@ import wikipedia
 from textblob import TextBlob as tb
 
 def tf(word, blob):
-    return blob.words.count(word) / len(blob.words)
+    x = blob.words.count(word)
+    y = len(blob.words)
+    #print x
+    #print y
+    return x/(y*1.0)
 
 def n_containing(word, bloblist):
     return sum(1 for blob in bloblist if word in blob)
 
 def idf(word, bloblist):
-    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+    x =float(1+ len(bloblist) / (1 + n_containing(word, bloblist)))
+    return math.log(x)
+    #print x
+    #try :
+    #    y = math.log(x)
+    #except ValueError:
+    #    y = 0
+    #return y
 
 def tfidf(word, blob, bloblist):
-    return tf(word, blob) * idf(word, bloblist)
+    x = tf(word, blob) 
+    y = idf(word, bloblist)
+    #print x
+    #print y
+    return x*y
 
 wikipedia.set_lang("fr")
 titles = wikipedia.random(pages=10)
 
-def pages(titles):
+def page(titles):
     db = []
     for i in titles:
         try :
@@ -35,12 +50,18 @@ def pages(titles):
             #x, y = inst.args
             #print 'x = ' , x
             #print 'y = ' , y
-            db.append(inst.options[0])
+            db.append(wikipedia.WikipediaPage(inst.options[0]))
     return db
         
-pages = pages(titles)
+pages = page(titles)
+
+#print(pages[0])
 
 contents = [i.content for i in pages]
+
+#for i in contents:
+#    print i
+#    print "-----------------------------\n\n\n\n"
 
 bloblist = [tb(i) for i in contents]
 
@@ -49,8 +70,14 @@ bloblist = [tb(i) for i in contents]
 for i, blob in enumerate(bloblist):
     print("Top words in document {}".format(i + 1))
     scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+    #l = []
+    #for i in scores:
+    #    if scores[i] < 0.001 :
+    #        l.append[i]
+    #for i in l:
+    #    del scores[i]
     sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-    for word, score in sorted_words[:3]:
-        print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+    for word, score in sorted_words[:5]:
+        print("\tWord: {}, TF-IDF: {}".format(word.encode(errors='ignore'), format(score, '.5f')))
 
 
