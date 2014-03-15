0e412e41717c9aa3671c3f5edfdffa6f9610c806
JCNM <jcharles.nmbiada@me.com>
2014-03-15 01:16:55 +0100

    Simple MAJ

--- Model.py
+++ Model.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import json as js
 import wikipedia as wiki
-
 import Article as ar
 from pymongo import MongoClient
 class Model:
@@ -10,15 +9,9 @@ class Model:
     def __init__(self, dbhost = "localhost", dbport=27017):
         
         self.client = MongoClient(dbhost, dbport)
-        self.db = self.client.test_database
+        self.db = self.client.hotspot
         self.articles = self.db.articles
-        #self.tfidf = tf.TfIdf([])
-        arti = ar.Article("Un article", False, [("mot1", 0.34), ("mot2", 0.5)])
-        articles.insert(arti.__dict__)
         
-    #def getCurrentTF():
-        #return self.tfidf
-
     def insert(article): # possibilite d'inserre plusieurs articles si article est une liste d article
         return self.articles.insert(article.__dict__) #return le object id de l'insertion
 
